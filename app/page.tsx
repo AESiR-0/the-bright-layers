49e2e369ff4ab1d1756d17fbb5340d59409d5846
AESiR <cyborgkiller1008@gmail.com>
2024-08-02 01:44:17 +0530

    first

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <p>Hey I am garet</p>
+    <p className="font-bold">hey I am garet bold</p>
+   </div>
+  );
+}
